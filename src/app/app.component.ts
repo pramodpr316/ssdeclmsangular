a3f525b9902c90ca02d7d9dca0b1f9845b64b2a9
gj1153 <pramodpr316@users.noreply.github.com>
2020-04-08 19:13:05 -0400

    Angular Day One

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lmsdecangular';
+  title = 'LMS Angular for SS Dec 2019';
 }
