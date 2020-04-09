66abffc2bda5171661c35e694083ee7dd2375c31
gj1153 <pramodpr316@users.noreply.github.com>
2020-04-09 18:15:32 -0400

    Commit #2: Added Routing, Layout, Home, Admin & other components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'lms/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'lms/home',
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { LibrarianComponent } from './librarian/librarian.component';
+import { BorrowerComponent } from './borrower/borrower.component';
+import { AuthorComponent } from './admin/author/author.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    LibrarianComponent,
+    BorrowerComponent,
+    AuthorComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
